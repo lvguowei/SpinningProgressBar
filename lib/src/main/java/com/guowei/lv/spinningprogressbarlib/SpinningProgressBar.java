8aa22c743ba552630f547652f23dc15b61b451ca
lvguowei <lvguowei1002@gmail.com>
2016-12-28 12:33:55 +0200

    extends ImageView for better layout

--- lib/src/main/java/com/guowei/lv/spinningprogressbarlib/SpinningProgressBar.java
+++ lib/src/main/java/com/guowei/lv/spinningprogressbarlib/SpinningProgressBar.java
@@ -3,33 +3,28 @@ package com.guowei.lv.spinningprogressbarlib;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
-
-public class SpinningProgressBar extends View {
+/**
+ * Add spinning animation to the image view
+ */
+public class SpinningProgressBar extends ImageView {
 
     private static final int DEFAULT_CYCLE_DURATION = 800;
     private static final int MODE_LINEAR = 0;
     private static final int MODE_DECELERATE = 1;
-
-    private Bitmap image;
 
     private int cycleDuration;
 
     private boolean clockwise;
 
     private int mode;
-
-    private Paint paint;
 
     private ObjectAnimator animation;
 
@@ -44,34 +39,17 @@ public class SpinningProgressBar extends View {
     }
 
     private void init(Context context, AttributeSet attrs) {
-        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        image = BitmapFactory.decodeResource(getResources(), R.drawable.default_image);
         cycleDuration = DEFAULT_CYCLE_DURATION;
         clockwise = true;
         mode = MODE_LINEAR;
+
         if (attrs != null) {
             final TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.SpinningProgressBar, 0, 0);
-            int resId = array.getResourceId(R.styleable.SpinningProgressBar_srcImg, R.drawable.default_image);
-            image = BitmapFactory.decodeResource(getResources(), resId);
             cycleDuration = array.getInteger(R.styleable.SpinningProgressBar_cycleDuration, DEFAULT_CYCLE_DURATION);
             clockwise = array.getBoolean(R.styleable.SpinningProgressBar_clockwise, true);
             mode = array.getInt(R.styleable.SpinningProgressBar_mode, MODE_LINEAR);
             array.recycle();
         }
-    }
-
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int width = image.getWidth();
-        int height = image.getHeight();
-        setMeasuredDimension(width, height);
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawBitmap(image, 0, 0, paint);
     }
 
     @Override
@@ -82,8 +60,8 @@ public class SpinningProgressBar extends View {
 
     @Override
     protected void onDetachedFromWindow() {
-        stopAnimation();
         super.onDetachedFromWindow();
+        stopAnimation();
     }
 
     @Override
